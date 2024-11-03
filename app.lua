43617f043172b86aeb25aed921128a6a40d2c6c3
Avi <avibanerjee2000@gmail.com>
2024-11-03 17:54:04 +0530

    .

--- app.lua
+++ app.lua
@@ -1,8 +1,32 @@
-local a, b = 1, 2
+-- local mod = require('chip')
 
-local z = a*a + b*b
+print('1. add')
+print('2. sub')
+print('3. div')
+print('4. mult')
+print('--------')
+print('select an option (1, 2, 3, 4): ') local option = io.read("*n")
 
---[[
-  z = 8
---]]
-print(a..b..z)
+print('type the first number: ') local num1 = io.read("*n")
+print('type the second number: ') local num2 = io.read("*n")
+
+local function doMath()
+  if option == 1 then
+    return num1 + num2
+  end
+  
+  if option == 2 then
+    return num1 - num2
+  end
+  
+  if option == 3 then
+    return num1 / num2
+  end
+  
+  if option == 4 then
+    return num1 * num2
+  end
+end
+
+io.write("The result is: ", doMath())
+
---
+++ chip.lua
@@ -0,0 +1,12 @@
+local chip = [[
+Now, baby, I'm the ripper
+Your baby daddy's worst nightmare
+Catch me by the Clair, I'll be right there
+Niggas copy everything we say
+Louie frames, eyes lower than my GPA (Uh!)
+Riding and swervin', kush, I'm blown
+Doors wide, my trunk bump like Eddy Road (Haha)
+I don't cake hoes, I never tip the stripper
+]]
+
+print(chip)
